86364e9b64bc2a47d4abb6426bb94d597b1aeb45
Mattia Astorino <astorino.mattia@gmail.com>
2020-01-19 17:54:34 +0100

    chore: Remove stimulus

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,4 @@
-import { Application } from 'stimulus'
 import Turbolinks from 'turbolinks'
-
-/**
- * Custom JS components/controllers powered by Stimulus
- */
-import HelloController from './controllers/hello_controller'
-
-/**
- * Init Simulus library
- */
-const application = Application.start()
-
-/**
- * Register Stimulus components/Controllers
- */
-application.register('hello', HelloController)
 
 /**
  * Init Turbolinks within the site
